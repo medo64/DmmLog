914e8f83998ed7e7d29e4ad65fc36173bb969d22
Josip Medved <jmedved@jmedved.com>
2014-04-07 20:15:04 -0700

    Added licence.

--- Setup/Publish.cmd
+++ Setup/Publish.cmd
@@ -4,17 +4,32 @@ SET        FILE_SETUP=".\DmmLog.iss"
 SET     FILE_SOLUTION="..\Source\DmmLog.sln"
 SET  FILES_EXECUTABLE="..\Binaries\DmmLog.exe" "..\Binaries\DmmLogBaseDriver.dll" "..\Binaries\DmmLogRandomDriver.dll" "..\Binaries\DmmLogAgilentDriver.dll"
 SET       FILES_OTHER="..\Binaries\ReadMe.txt"
+SET     FILES_LICENSE="License.txt"
 
-SET      COMPILE_TOOL="%PROGRAMFILES(X86)%\Microsoft Visual Studio 11.0\Common7\IDE\devenv.exe"
+SET    COMPILE_TOOL_1="%PROGRAMFILES(X86)%\Microsoft Visual Studio 12.0\Common7\IDE\devenv.exe"
+SET    COMPILE_TOOL_2="%PROGRAMFILES(X86)%\Microsoft Visual Studio 12.0\Common7\IDE\WDExpress.exe"
 SET        SETUP_TOOL="%PROGRAMFILES(x86)%\Inno Setup 5\iscc.exe"
 
 SET         SIGN_TOOL="%PROGRAMFILES(X86)%\Windows Kits\8.0\bin\x86\signtool.exe"
-SET         SIGN_HASH="EB41D6069805B20D87219E0757E07836FB763958"
+SET         SIGN_HASH="C02FF227D5EE9F555C13D4C622697DF15C6FF871"
 SET SIGN_TIMESTAMPURL="http://timestamp.comodoca.com/rfc3161"
 
 
 ECHO --- BUILD SOLUTION
 ECHO.
+
+IF EXIST %COMPILE_TOOL_1% (
+    ECHO Using Visual Studio
+    SET COMPILE_TOOL=%COMPILE_TOOL_1%
+) ELSE (
+    IF EXIST %COMPILE_TOOL_2% (
+        ECHO Using Visual Studio Express
+        SET COMPILE_TOOL=%COMPILE_TOOL_2%
+    ) ELSE (
+        ECHO Cannot find Visual Studio!
+        PAUSE && EXIT /B 255
+    )
+)
 
 RMDIR /Q /S "..\Binaries" 2> NUL
 %COMPILE_TOOL% /Build "Release" %FILE_SOLUTION%
@@ -63,7 +78,7 @@ ECHO --- RENAME LATEST
 ECHO.
 
 SET _OLDSETUPEXE=%_SETUPEXE%
-SET _SETUPEXE=%_SETUPEXE:000=LATEST%
+SET _SETUPEXE=%_SETUPEXE:000=-LATEST%
 IF NOT %_OLDSETUPEXE%==%_SETUPEXE% (
     ECHO Renaming %_OLDSETUPEXE% to %_SETUPEXE%
     MOVE .\Temp\%_OLDSETUPEXE% .\Temp\%_SETUPEXE%
@@ -98,7 +113,7 @@ ECHO.
 
 SET _SETUPZIP=%_SETUPEXE:.exe=.zip%
 ECHO Zipping into %_SETUPZIP%
-"%PROGRAMFILES%\WinRAR\WinRAR.exe" a -afzip -ep -m5 ".\Temp\%_SETUPZIP%" %FILES_EXECUTABLE% %FILES_OTHER%
+"%PROGRAMFILES%\WinRAR\WinRAR.exe" a -afzip -ep -m5 -z%FILES_LICENSE% ".\Temp\%_SETUPZIP%" %FILES_EXECUTABLE% %FILES_OTHER%
 IF ERRORLEVEL 1 PAUSE && EXIT /B %ERRORLEVEL%
 
 ECHO.
